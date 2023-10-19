4e62db87dde5dc1b174a343c5d014d08beaa2381
Gabriel <gabriellagrota23@gmail.com>
2023-10-19 09:28:54 -0300

    Feat: Realizada criação da estrutura do Discovery Server da aplicação

---
+++ src/test/java/br/com/rcbr/svc/discoveryserver/DiscoveryServerTests.java
@@ -0,0 +1,13 @@
+package br.com.rcbr.svc.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
